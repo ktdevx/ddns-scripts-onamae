dc2fae337c979f53b338c31e1abbcf8d29e859a8
Kaito Takemura <ktdevx@gmail.com>
2025-05-11 00:09:04 +0900

    Add DDNS script

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ddns-scripts-onamae
+
+OpenWrt/LEDE Dynamic DNS (DDNS) Client scripts extension for 'onamae.com'.
+
+## Configurations
+
+- domain: hostname@yourdomain.TLD
+- username: onamae.com Navi onamae ID
+- password: onamae.com Navi password
+
+## Depends
+
+- ddns-scripts
+- openssl-util
---
+++ files/usr/lib/ddns/update_onamae_com.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+local OPENSSL=$(command -v openssl)
+
+[ -z "$OPENSSL" ] && write_log 14 "Onamae.com communication require openssl. Please install"
+[ -z "$domain" ] && write_log 14 "Service section not configured correctly! Missing 'domain'"
+[ -z "$username" ] && write_log 14 "Service section not configured correctly! Missing 'username'"
+[ -z "$password" ] && write_log 14 "Service section not configured correctly! Missing 'password'"
+
+local __HOST=$(printf %s "$domain" | cut -d@ -f1)
+local __DOMAIN=$(printf %s "$domain" | cut -d@ -f2)
+
+local __MSG="LOGIN
+USERID:$username
+PASSWORD:$password
+.
+MODIP"
+if [ -n "$__HOST" ]; then
+	__MSG="$__MSG
+	HOSTNAME:$__HOST"
+fi
+__MSG="$__MSG
+DOMNAME:$__DOMAIN
+IPV4:$__IP
+.
+LOGOUT
+.
+"
+
+local __RESULT_MSG=$(printf "%s" $__MSG | $OPENSSL s_client -quiet ddnsclient.onamae.com:65010)
+
+local __SUCCESS_MSG="000 COMMAND SUCCESSFUL
+.
+000 COMMAND SUCCESSFUL
+.
+000 COMMAND SUCCESSFUL
+.
+000 COMMAND SUCCESSFUL
+."
+
+if [ "$__RESULT_MSG" != "$__SUCCESS_MSG" ]; then
+	write_log 14 "Failed to update IP address"
+fi
+
+return 0
